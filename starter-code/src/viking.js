82e2ddf9e51790cd7c46c1aea78c2a935fb61b65
misanc21 <misanc21@gmail.com>
2019-12-03 22:00:40 -0600

    viking part 1

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -9,11 +9,26 @@ class Soldier {
     }
     receiveDamage(damage) {
         this.health -= damage;
+        //return `${name} has received ${damage} points of damage`
     }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        let message = ''
+        this.health -= damage
+        this.health > 0 ?
+            message = `${this.name} has received ${damage} points of damage` : message = `${this.name} has died in act of combat`
+        return message
+    }
+
+}
 
 // Saxon
 class Saxon {}
